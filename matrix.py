76df32fa4dda4d6bd9662b9378dc45ebdbee2c75
Sahajadee Reba <52814449+sreba@users.noreply.github.com>
2020-02-18 15:42:43 -0500

    Second Project_Udacity

---
+++ matrix.py
@@ -0,0 +1,233 @@
+import math
+from math import sqrt
+import numbers
+
+def zeroes(height, width):
+        """
+        Creates a matrix of zeroes.
+        """
+        g = [[0.0 for _ in range(width)] for __ in range(height)]
+        return Matrix(g)
+
+def identity(n):
+        """
+        Creates a n x n identity matrix.
+        """
+        I = zeroes(n, n)
+        for i in range(n):
+            I.g[i][i] = 1.0
+        return I
+
+class Matrix(object):
+
+    # Constructor
+    def __init__(self, grid):
+        self.g = grid
+        self.h = len(grid)
+        self.w = len(grid[0])
+
+    #
+    # Primary matrix math methods
+    #############################
+ 
+    def determinant(self):
+        """
+        Calculates the determinant of a 1x1 or 2x2 matrix.
+        """
+        if not self.is_square():
+            raise(ValueError, "Cannot calculate determinant of non-square matrix.")
+        if self.h > 2:
+            raise(NotImplementedError, "Calculating determinant not implemented for matrices largerer than 2x2.")
+        
+        # TODO - your code here
+        
+        if self.h == 1:
+            return self[0][0]
+        
+        elif self.h == 2:
+            a = self[0][0]
+            b = self[0][1]
+            c = self[1][0]
+            d = self[1][1]
+            return a*d - b*c
+        
+        else:
+            return none
+
+    def trace(self):
+        """
+        Calculates the trace of a matrix (sum of diagonal entries).
+        """
+        if not self.is_square():
+            raise(ValueError, "Cannot calculate the trace of a non-square matrix.")
+
+        # TODO - your code here
+        Trace=0
+        for i in range(len(self.g)):
+            for j in range(len(self.g)):
+                if i == j:
+                    Trace += self.g[i][j]
+        return Trace
+
+    def inverse(self):
+        """
+        Calculates the inverse of a 1x1 or 2x2 Matrix.
+        """
+        if not self.is_square():
+            raise(ValueError, "Non-square Matrix does not have an inverse.")
+        if self.h > 2:
+            raise(NotImplementedError, "inversion not implemented for matrices larger than 2x2.")
+            
+        det = self.determinant()
+        if det == 0:
+            raise(ValueError, "This Matrix doesnot have an inverse.")
+        
+        if self.h == 1:
+            return Matrix([[1/self.g[0][0]]])
+        elif self.h == 2:
+            inv_matrix = zeroes(self.h, self.w)
+            inv_matrix = zeroes(self.h, self.w)
+            inv_matrix.g[0][0] = self.g[1][1] / det
+            inv_matrix.g[1][1] = self.g[0][0] / det
+            inv_matrix.g[0][1] = -self.g[0][1] / det
+            inv_matrix.g[1][0] = -self.g[1][0] / det
+            return inv_matrix
+
+            
+ 
+
+    def T(self):
+        """
+        Returns a transposed copy of this Matrix.
+        """
+        # TODO - your code here
+        transpose = zeroes(self.h, self.w)
+        
+        for r in range(self.h):
+            for c in range(self.w):
+                transpose[r][c] = self.g[c][r]
+            
+        return transpose
+
+    def is_square(self):
+        return self.h == self.w
+
+    #
+    # Begin Operator Overloading
+    ############################
+    def __getitem__(self,idx):
+        """
+        Defines the behavior of using square brackets [] on instances
+        of this class.
+
+        Example:
+
+        > my_matrix = Matrix([ [1, 2], [3, 4] ])
+        > my_matrix[0]
+          [1, 2]
+
+        > my_matrix[0][0]
+          1
+        """
+        return self.g[idx]
+
+    def __repr__(self):
+        """
+        Defines the behavior of calling print on an instance of this class.
+        """
+        s = ""
+        for row in self.g:
+            s += " ".join(["{} ".format(x) for x in row])
+            s += "\n"
+        return s
+
+    def __add__(self,other):
+       
+        if self.h != other.h or self.w != other.w:
+            raise(ValueError, "Matrices can only be added if the dimensions are the same") 
+        
+        # initialize matrix to hold the results
+        matrixSum = zeroes(self.h, self.w)
+    
+        for r in range(self.h):
+            for c in range(self.w):
+                matrixSum[r][c] = self.g[r][c] + other.g[r][c]
+           
+        return matrixSum
+
+    def __neg__(self):
+        """
+        Defines the behavior of - operator (NOT subtraction)
+
+        Example:
+
+        > my_matrix = Matrix([ [1, 2], [3, 4] ])
+        > negative  = -my_matrix
+        > print(negative)
+          -1.0  -2.0
+          -3.0  -4.0
+        """
+        neg_matrix = zeroes(self.h, self.w)
+        for r in range(self.h): 
+            for c in range(self.w): 
+                neg_matrix[r][c] = neg_matrix.g[r][c] - self.g[r][c] 
+        return neg_matrix
+    
+
+
+    def __sub__(self, other):
+        """
+        Defines the behavior of - operator (as subtraction)
+        """
+        matrixSub = zeroes(self.h,self.w)
+        
+        # For loop within a for loop to iterate over the matrices
+        for r in range(self.h):
+            for c in range(self.w):
+                matrixSub[r][c] = self.g[r][c] - other.g[r][c]
+                
+        return matrixSub
+
+
+    def __mul__(self, other):
+        """
+        Defines the behavior of * operator (matrix multiplication)
+         """
+   
+                
+          
+        multi = zeroes(self.h, other.w)
+        
+        for r in range(self.h):
+            for c in range(other.w):
+                for d in range(other.h):
+                    multi[r][c] += self.g[r][d] * other.g[d][c]
+        return multi
+       
+                
+      
+    def __rmul__(self, other):
+        """
+        Called when the thing on the left of the * is not a matrix.
+
+        Example:
+
+        > identity = Matrix([ [1,0], [0,1] ])
+        > doubled  = 2 * identity
+        > print(doubled)
+          2.0  0.0
+          0.0  2.0
+        """
+        if isinstance(other, numbers.Number):
+            pass
+            #   
+            # TODO - your code here
+            #
+            rmulti = []
+            for r in range(self.h):
+                row = []
+                for c in range(self.w):
+                    row.append(other*self[r][c])
+                rmulti.append(row)
+            return Matrix(rmulti)
+            
